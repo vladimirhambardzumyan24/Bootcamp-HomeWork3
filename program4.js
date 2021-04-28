b9a5a7c87268a0ffab78489dbf6f16f24d532c5d
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-04-28 16:03:24 +0400

    Swap two variables without using the third one

---
+++ program4.js
@@ -0,0 +1,16 @@
+//arajadranq 4
+let e = 10;
+d = 3;
+e = e - d; //7
+d = e + d; //10
+e = d - e;
+//tarberak 2
+let h = 10;
+let j = 3;
+h = h / j;
+j = h * j;
+h = j / h;
+//tarberak 3
+let s = 10;
+let q = 3;
+[s,q]=[q,s]
